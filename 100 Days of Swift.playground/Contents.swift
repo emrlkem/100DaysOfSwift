1ec9c7aaab60b24fe2dc6a5bd72e0d98ae677ea4
Emre <emre.ulkem@outlook.com>
2022-10-14 21:29:29 +0300

    Initial Commit

---
+++ 100 Days of Swift.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "Goodbye"
+
+var age = 25
+var population = 5_555_555
+
+var str1 = """
+This is \
+my first \
+multi-line.
+"""
+
+var pi = 3.14
+
+var awesome = false
+
+var score = 90
+var str2 = "My score is \(score)"
+var result = "The test result is here: \(str2)"
+
+//The let keyword creates constants, which are values that can be set once and never again. It’s preferable to use constants as often as possible.
+let taylor = "swift"
+
+let team: String = "Fenerbahce"
+let year: Int = 1907
+let lastMatch: Double = 3.2
+let matchWin: Bool = true

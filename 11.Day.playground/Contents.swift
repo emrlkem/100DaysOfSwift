2244a33a14a3d6a65b0f3a6d1d8b3f5b9d82ef96
Emre Ülkem <98457972+emrlkem@users.noreply.github.com>
2022-10-29 01:00:26 +0300

    Add files via upload

---
+++ 11.Day.playground/Contents.swift
@@ -0,0 +1,91 @@
+import UIKit
+
+
+    // Protocols
+
+protocol Identifiable {
+    var id: String { get set }
+}
+
+struct User: Identifiable {
+    var id: String
+}
+
+func displayID(thing: Identifiable) {
+    print("My ID is \(thing.id)")
+}
+
+
+    // Protocol Inheritance
+
+protocol Payable {
+    func calculateWages() -> Int
+}
+
+protocol NeedsTraining {
+    func study()
+}
+
+protocol HasVacation {
+    func takeVacation(days: Int)
+}
+
+protocol Employee: Payable, NeedsTraining, HasVacation { }
+
+
+    // Extensions
+
+extension Int {
+    func squared() -> Int {
+        return self * self
+    }
+}
+
+let number = 8
+number.squared()
+
+extension Int {
+    var isEven: Bool {
+        return self % 2 == 0
+    }
+}
+
+
+    // Protocol Extensions
+
+let pythons = ["Eric", "Emre", "Graham", "John", "Terry", "Mike"]
+let beatles = Set(["John", "Paul", "George", "Ringo"])
+
+extension Collection {
+    func summarize() {
+        print("There are \(count) of us:")
+        
+        for name in self {
+            print(name)
+        }
+    }
+}
+
+pythons.summarize()
+beatles.summarize()
+
+
+    // Protocol-Oriented Programming
+
+protocol Identiable {
+    var id: String { get set }
+    func identify()
+}
+
+extension Identifiable {
+    func identify() {
+        print("My ID is \(id).")
+    }
+}
+
+struct User1: Identifiable {
+    var id: String
+}
+
+let twostraws = User1(id: "twostraws")
+twostraws.identify()
